4ef1d68252da7163e0c35a1287b1e60ee0c2f65b
nw313009 <95502565+nw313009@users.noreply.github.com>
2022-05-25 13:31:20 -0500

    test

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -4,8 +4,8 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import Dashboard from "../dashboard/Dashboard";
 import NotFound from "./NotFound";
 import { today } from "../utils/date-time";
-import  useQuery  from "../utils/useQuery";
-import useQuery from "../utils/useQuery";
+
+
 /**
  * Defines all the routes for the application.
  *
@@ -14,9 +14,7 @@ import useQuery from "../utils/useQuery";
  * @returns {JSX.Element}
  */
 function Routes() {
-//comment to check
-  const useQuery = useQuery();
-  const date = query.get("date");
+
   return (
     <Switch>
       <Route exact={true} path="/">
